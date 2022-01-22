b94062c621907bada56b718e65da7dd296bf2cff
waterswat <waterswat@outlook.com>
2022-01-22 14:46:45 -0500

    init

---
+++ Send.mjs
@@ -0,0 +1,94 @@
+import http from 'http';
+import https from 'https';
+import { MapHeaderNamesFromArray, RawHeaderNames } from './HeaderUtil.mjs';
+
+// max of 4 concurrent sockets, rest is queued while busy? set max to 75
+// const http_agent = http.Agent();
+// const https_agent = https.Agent();
+
+export async function Fetch(server_request, request_headers, url){
+	const options = {
+		host: url.host,
+		port: url.port,
+		path: url.path,
+		method: server_request.method,
+		headers: request_headers,
+	};
+	
+	var request_stream;
+	
+	var response_promise = new Promise((resolve, reject) => {
+		try{
+			if(url.protocol == 'https:')request_stream = https.request(options, resolve);
+			else if(url.protocol == 'http:')request_stream = http.request(options, resolve);
+			else return reject(new RangeError(`Unsupported protocol: '${url.protocol}'`));
+			
+			request_stream.on('error', reject);
+		}catch(err){
+			reject(err);
+		}
+	});
+
+	if(request_stream){
+		server_request.pipe(request_stream);
+	}
+	
+	return await response_promise;
+}
+
+export async function SendBare(server, server_request, server_response){
+	const request_headers = Object.setPrototypeOf({}, null);
+	const response_headers = Object.setPrototypeOf({}, null);
+
+	response_headers['x-robots-tag'] = 'noindex';
+	response_headers['access-control-allow-headers'] = '*';
+	response_headers['access-control-allow-origin'] = '*';
+	response_headers['access-control-expose-headers'] = '*';
+	
+	if(server_request.method == 'OPTIONS'){
+		server_response.writeHead(200, response_headers);
+		return void server_response.end();
+	}
+
+	if('x-tomp-headers' in server_request.headers){
+		const json = JSON.parse(server_request.headers['x-tomp-headers']);
+		Object.assign(request_headers, json);
+	}
+
+	for(let header in server_request.headers){
+		if(header.startsWith('accept')){
+			request_headers[header] = server_request.headers[header];
+		}
+	}
+
+	const search = new URLSearchParams(server_request.url.slice(server_request.url.indexOf('?')));
+	
+	try{
+		var url = JSON.parse(search.get('url'));
+	}catch(err){
+		return void server.send_json(server_response, 400, { message: messages['error.badbody'] });
+	}
+
+	if(!url)return void server.send_json(server_response, 400, { message: messages['error.nourlquery'] });
+	
+	// todo: do same procedure chrome does for capitalizing headers for http/1.0 - http/1.1 servers
+	// wont recover all capitalization eg headers set by XMLHttpRequest
+
+	try{
+		var response = await Fetch(server_request, request_headers, url);
+	}catch(err){
+		console.error(err, url);
+		throw err;
+	}
+
+	for(let header in response.headers){
+		if(header == 'content-encoding' || header == 'x-content-encoding')response_headers['content-encoding'] = response.headers[header];
+		else if(header == 'content-length')response_headers['content-length'] = response.headers[header];
+	}
+
+	response_headers['x-tomp-headers'] = JSON.stringify(MapHeaderNamesFromArray(RawHeaderNames(response.rawHeaders), {...response.headers}));
+	response_headers['x-tomp-status'] = response.statusCode
+	response_headers['x-tomp-status-text'] = response.statusMessage;
+	server_response.writeHead(200, response_headers);
+	response.pipe(server_response);
+}
---
+++ Server.mjs
@@ -0,0 +1,54 @@
+import { SendBare } from './Send.mjs';
+
+export class Server {
+	prefix = '';
+	constructor(config = {}){
+		if(typeof config.prefix != 'string'){
+			throw new Error('Prefix must be specified.')
+		}
+
+		this.prefix = config.prefix;
+	}
+	upgrade(req, socket, head){
+		socket.end();
+	}
+	send_json(response, status, json){
+		const send = Buffer.from(JSON.stringify(json));
+		response.writeHead(status, { 
+			'content-type': 'application/json',
+			'content-length': send.byteLength,
+		});
+		
+		// console.trace(json);
+
+		response.end(send);
+	}
+	route_request(request, response){
+		if(request.url.startsWith(this.prefix)){
+			this.request(request, response);
+			return true;
+		}else{
+			return false;
+		}
+	}
+	async request(request, response){
+		let finished = false;
+
+		response.on('finish', () => finished = true);
+		
+		response.on('error', error => {
+			console.error(error);
+		});
+
+		try{
+			return void await SendBare(this, request, response);
+		}catch(err){
+			setTimeout(async () => {
+				console.error(err);
+				if(!finished)return void await this.send_json(response, 400, {
+					message: `TOMPServer encountered an exception while handling your request. Contact this server's administrator.`,
+				});
+			});
+		}
+	}
+};
