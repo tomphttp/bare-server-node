28ddb884308fc785a1623a48f376335fea02c232
e9x <x@sys32.dev>
2022-02-04 16:09:57 -0500

    new websocket protocol

--- V1.mjs
+++ V1.mjs
@@ -1,8 +1,10 @@
-import http, { request } from 'http';
-import https from 'https';
+import http from 'node:http';
+import https from 'node:https';
+import { WebSocketServer, WebSocket } from 'ws';
 import { MapHeaderNamesFromArray, RawHeaderNames } from './HeaderUtil.mjs';
 import { decode_protocol } from './EncodeProtocol.mjs';
 import { Response } from './Response.mjs';
+
 
 // max of 4 concurrent sockets, rest is queued while busy? set max to 75
 // const http_agent = http.Agent();
@@ -152,79 +154,74 @@ export async function v1(server_request){
 	return new Response(response, 200, response_headers);
 }
 
-export async function v1socket(server, server_request, server_socket, server_head){
-	if(!server_request.headers['sec-websocket-protocol']){
-		return socket.end();
+const wss = new WebSocketServer({
+	noServer: true,
+});
+
+const default_ports = [80, 443];
+
+function remote_toString(remote){
+	let port_string;
+
+	if(default_ports.includes(remote.port)){
+		port_string = '';
+	}else{
+		port_string = `:${remote.port}`;
 	}
 
-	const [ first_protocol, data ] = server_request.headers['sec-websocket-protocol'].split(/,\s*/g);
-	
-	const {
-		remote,
-		headers,
-		forward_headers,
-	} = JSON.parse(decode_protocol(data));
-	
-	load_forwarded_headers(server_request, forward_headers, headers);
+	return `${remote.protocol}//${remote.host}${port_string}${remote.path}`;
+}
 
-	const options = {
-		host: remote.host,
-		port: remote.port,
-		path: remote.path,
-		headers,
-		method: server_request.method,	
-	};
-	
-	let request_stream;
-	
-	let response_promise = new Promise((resolve, reject) => {
-		try{
-			if(remote.protocol === 'wss:')request_stream = https.request(options, res => reject(`Remote didn't upgrade the request`));
-			else if(remote.protocol === 'ws:')request_stream = http.request(options, res => reject(`Remote didn't upgrade the request`));
-			else return reject(new RangeError(`Unsupported protocol: '${remote.protocol}'`));
-			
-			request_stream.on('upgrade', (...args) => resolve(args))
-			request_stream.on('error', reject);
-			request_stream.write(server_head);
-			request_stream.end();
-		}catch(err){
-			reject(err);
-		}
+
+async function v1connection(ws, server_request){
+	ws.once('message', data => {
+		const { remote, headers, forward_headers, protocols } = JSON.parse(data.toString());
+
+		load_forwarded_headers(server_request, forward_headers, headers);
+
+		const remote_ws = new WebSocket(remote_toString(remote), protocols, {
+			headers,
+		});
+
+		ws.on('close', () => {
+			remote_ws.close();
+		});
+
+		ws.on('message', (data, binary) => {
+			remote_ws.send(binary ? data : data.toString());
+		});
+
+		remote_ws.on('message', (data, binary) => {
+			ws.send(binary ? data : data.toString());
+		});
+
+		remote_ws.on('close', () => {
+			ws.close();
+		});
+
+		remote_ws.on('error', error => {
+			console.log(error);
+			ws.close();
+		});
+
+		let response;
+
+		remote_ws.on('upgrade', r => {
+			response = r;
+		});
+
+		remote_ws.on('open', () => {
+			ws.send(JSON.stringify({
+				headers: MapHeaderNamesFromArray(RawHeaderNames(response.rawHeaders), {...response.headers}),
+				protocol: remote_ws.protocol,
+				extensions: remote_ws.extensions,
+			}));
+		});
 	});
+}
 
-	const [ response, socket, head ] = await response_promise;
-	
-	const response_headers = [
-		'HTTP/1.1 101 Switching Protocols',
-		'Upgrade: websocket',
-		'Connection: Upgrade',
-		'Sec-WebSocket-Accept: ' + response.headers['sec-websocket-accept'],
-		'Sec-WebSocket-Protocol: ' + first_protocol,
-	].concat('', '').join('\r\n');
-
-	server_socket.write(response_headers);
-	server_socket.write(head);
-
-	socket.on('close', () => {
-		console.log('Remote closed');
-		server_socket.end();
+export async function v1socket(server, server_request, server_socket, server_head){
+	wss.handleUpgrade(server_request, server_socket, server_head, ws => {
+		v1connection(ws, server_request);
 	});
-
-	server_socket.on('close', () => {
-		console.log('Serving closed');
-		socket.end();
-	});
-
-	socket.on('error', err => {
-		console.error('Remote socket error:', err);
-		server_socket.end();
-	});
-	
-	server_socket.on('error', err => {
-		console.error('Serving socket error:', err);
-		socket.end();
-	});
-
-	socket.pipe(server_socket);
-	server_socket.pipe(socket);
 }
