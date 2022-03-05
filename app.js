a427c0b2b6453529f63b9f820ce109ce3799621c
David Reed <x@sys32.dev>
2022-03-05 18:01:09 -0500

    update cli tool

--- app.js
+++ app.js
@@ -1,42 +1,15 @@
-import BareServer from './Server.js';
-import { Server as HTTPServer } from 'node:http';
-import { program, Option } from 'commander';
+import { Command, Option } from 'commander';
+import server from './cli/server.js';
+
+const program = new Command();
 
 program
+.command('server')
 .addOption(new Option('--d, --directory <string>', 'Bare directory').default('/'))
 .addOption(new Option('--h, --host <string>', 'Listening host').default('localhost'))
 .addOption(new Option('--p, --port <number>', 'Listening port').default(80).env('PORT'))
 .addOption(new Option('--e, --errors', 'Error logging').default(false))
+.action(server)
 ;
 
 program.parse(process.argv);
-
-const options = program.opts();
-
-const bare = new BareServer(options.directory, options.errors);
-console.info('Created Bare Server on directory:', options.directory);
-console.info('Error logging is', options.errors ? 'enabled.' : 'disabled.');
-
-const http = new HTTPServer();
-console.info('Created HTTP server.');
-
-http.on('request', (req, res) => {
-	if(bare.route_request(req, res))return;
-
-	res.writeHead(400);
-	res.send('Not found');
-});
-
-http.on('upgrade', (req, socket, head) => {
-	if(bare.route_upgrade(req, socket, head))return;
-	socket.end();
-});
-
-http.on('listening', () => {
-	console.log(`HTTP server listening. View live at http://${options.host}:${options.port}${options.directory}`);
-});
-
-http.listen({
-	host: options.host,
-	port: options.port,
-});
