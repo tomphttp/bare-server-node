e16baf08af68f5006da08fc787aa04961b723297
David Reed <x@sys32.dev>
2022-05-25 18:47:53 -0400

    dotenv, make server an alias, 0.0.2

--- app.js
+++ app.js
@@ -1,23 +1,57 @@
 import { Command, Option } from 'commander';
-import server from './cli/server.js';
+import { config } from 'dotenv';
+import { Server as HTTPServer } from 'node:http';
+import BareServer from './Server.js';
+
+config();
 
 const program = new Command();
 
 program
-	.command('server')
-	.addOption(
-		new Option('--d, --directory <directory>', 'Bare directory').default('/')
+	.alias('server')
+	.option('--d, --directory <directory>', 'Bare directory', '/')
+	.option(
+		'--h, --host <host>',
+		'Listening host',
+		process.env.HOST || 'localhost'
 	)
-	.addOption(
-		new Option('--h, --host <host>', 'Listening host').default('localhost')
+	.option('--p, --port <port>', 'Listening port', process.env.PORT || 80)
+	.option('--e, --errors', 'Error logging', 'ERRORS' in process.env)
+	.option(
+		'--la, --local-address <address>',
+		'Address/network interface',
+		process.env.LOCAL_ADDRESS
 	)
-	.addOption(
-		new Option('--p, --port <port>', 'Listening port').default(80).env('PORT')
-	)
-	.addOption(new Option('--e, --errors', 'Error logging').default(false))
-	.addOption(
-		new Option('--la, --local-address <address>', 'Address/network interface')
-	)
-	.action(server);
+	.action(({ directory, errors, host, port, localAddress }) => {
+		const bare = new BareServer(directory, errors, localAddress);
+		console.info('Created Bare Server on directory:', directory);
+		console.info('Error logging is', errors ? 'enabled.' : 'disabled.');
+
+		const http = new HTTPServer();
+		console.info('Created HTTP server.');
+
+		http.on('request', (req, res) => {
+			if (bare.route_request(req, res)) return;
+
+			res.writeHead(400);
+			res.send('Not found');
+		});
+
+		http.on('upgrade', (req, socket, head) => {
+			if (bare.route_upgrade(req, socket, head)) return;
+			socket.end();
+		});
+
+		http.on('listening', () => {
+			console.log(
+				`HTTP server listening. View live at http://${host}:${port}${directory}`
+			);
+		});
+
+		http.listen({
+			host: host,
+			port: port,
+		});
+	});
 
 program.parse(process.argv);
