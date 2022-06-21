8df50c1bb00d9d6c9d16aa2729e6255661797bd0
David Reed <x@sys32.dev>
2022-06-21 12:31:51 -0400

    .cjs

--- rollup.config.js
+++ rollup.config.js
@@ -4,12 +4,12 @@ import nodeResolve from '@rollup/plugin-node-resolve';
 import typescript from 'rollup-plugin-typescript2';
 
 export default [
-	['esm', 'src/index.ts', 'named'], // import
-	['umd', 'src/createServer.ts', 'default'], // require
-].map(([format, input, exports]) => ({
+	['esm', 'src/index.ts', 'named', false], // import
+	['umd', 'src/createServer.ts', 'default', true], // require
+].map(([format, input, exports, cjs]) => ({
 	input,
 	output: {
-		file: `dist/BareServer.${format}.js`,
+		file: `dist/BareServer.${format}${cjs ? '.cjs' : '.js'}`,
 		format,
 		name: 'BareServer',
 		exports,
