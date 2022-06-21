de629f5dc5d3128e1388bf2ed5f13506dcd21d52
David Reed <x@sys32.dev>
2022-06-21 19:17:17 -0400

    server config

--- src/createServer.ts
+++ src/createServer.ts
@@ -1,10 +1,10 @@
-import BareServer, { BareServerInit } from './BareServer';
+import BareServer, { ServerConfig } from './BareServer';
 import registerV1 from './V1';
 import registerV2 from './V2';
 
 export default function createServer(
 	directory: string,
-	init: BareServerInit = {}
+	init: Partial<ServerConfig> = {}
 ) {
 	const server = new BareServer(directory, init);
 	registerV1(server);
