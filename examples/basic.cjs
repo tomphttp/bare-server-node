951839765a7e100197cbd32d8e3cddff975800d9
David Reed <reedswork@proton.me>
2022-09-28 16:41:36 -0400

    add create-react-app and basic commonjs examples

---
+++ examples/basic.cjs
@@ -0,0 +1,39 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const http = require('http');
+const createBareServer = require('@tomphttp/bare-server-node');
+
+const httpServer = http.createServer();
+
+const bareServer = createBareServer('/', {
+	logErrors: false,
+	localAddress: undefined,
+	maintainer: {
+		email: 'tomphttp@sys32.dev',
+		website: 'https://github.com/tomphttp/',
+	},
+});
+
+httpServer.on('request', (req, res) => {
+	if (bareServer.shouldRoute(req)) {
+		bareServer.routeRequest(req, res);
+	} else {
+		res.writeHead(400);
+		res.end('Not found.');
+	}
+});
+
+httpServer.on('upgrade', (req, socket, head) => {
+	if (bareServer.shouldRoute(req)) {
+		bareServer.routeUpgrade(req, socket, head);
+	} else {
+		socket.end();
+	}
+});
+
+httpServer.on('listening', () => {
+	console.log('HTTP server listening');
+});
+
+httpServer.listen({
+	port: 8080,
+});
---
+++ examples/setupProxy.cjs
@@ -0,0 +1,21 @@
+/**
+ * Example of using src/setupProxy.js to register a Bare server in your Create React App development server.
+ * See https://create-react-app.dev/docs/proxying-api-requests-in-development/
+ */
+/* eslint-disable @typescript-eslint/no-var-requires */
+const createBareServer = require('@tomphttp/bare-server-node');
+
+/**
+ * Entry point called by react-scripts during development (npm start)
+ * @param {import('express').Express} app
+ */
+function setupProxy(app) {
+	const bareServer = createBareServer('/bare/');
+
+	app.use((req, res, next) => {
+		if (bareServer.shouldRoute(req)) bareServer.routeRequest(req, res);
+		else next();
+	});
+}
+
+module.exports = setupProxy;
