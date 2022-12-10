f333188c167e9c973a08bcc6a6d1a9b9148195c8
David Reed <reedswork@proton.me>
2022-12-10 17:04:12 -0500

    correctly set status code

--- src/BareServer.ts
+++ src/BareServer.ts
@@ -198,11 +198,16 @@ export default class Server extends EventEmitter {
 				throw new createHttpError.NotFound();
 			}
 		} catch (error) {
-			if (this.options.logErrors) {
-				console.error(error);
-			}
+			if (this.options.logErrors) console.error(error);
 
-			if (error instanceof Error) {
+			if (createHttpError.isHttpError(error)) {
+				response = json(error.statusCode, {
+					code: 'UNKNOWN',
+					id: `error.${error.name}`,
+					message: error.message,
+					stack: error.stack,
+				});
+			} else if (error instanceof Error) {
 				response = json(500, {
 					code: 'UNKNOWN',
 					id: `error.${error.name}`,
